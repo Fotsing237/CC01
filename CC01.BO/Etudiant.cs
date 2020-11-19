451b71cbc8ca7ca85be078aea676afaed6cf95fb
Nkayoum <fares.nkayoum@2tcorp.org>
2020-11-19 14:26:17 +0100

    BO effectuer

---
+++ CC01.BO/Etudiant.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CC01.BO
+{
+    class Etudiant
+    {
+        public string Name { get; set; }
+        public string Matricule { get; set; }
+        public string Contact { get; set; }
+        public string School { get; set; }
+        public string PlaceofBirth { get; set; }
+        public string Birthdate { get; set; }
+        public string Mail { get; set; }
+
+        public Etudiant(string name, string matricule, string contact, string school, string placeofBirth, string birthdate, string mail)
+        {
+            Name = name;
+            Matricule = matricule;
+            Contact = contact;
+            School = school;
+            PlaceofBirth = placeofBirth;
+            Birthdate = birthdate;
+            Mail = mail;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Etudiant etudiant &&
+                   Matricule == etudiant.Matricule;
+        }
+
+        public override int GetHashCode()
+        {
+            return 797189699 + EqualityComparer<string>.Default.GetHashCode(Matricule);
+        }
+    }
+}
